33a6402d7555821c2a4fa0b04dc980d808085dab
Dheeraj bisht <48381903+deebisht98@users.noreply.github.com>
2022-06-04 18:23:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import Counter from "./features/counter/Counter";
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const inititalState = {
+  count: 0
+};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: inititalState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload;
+    }
+  }
+});
+
+export const {
+  increment,
+  decrement,
+  reset,
+  incrementByAmount
+} = counterSlice.actions;
+
+export default counterSlice.reducer;
